23d6f18a85652e5a4efa4754cbfb1dfbc4ab7c05
Jean Thomé <jtc.jean@gmail.com>
2019-08-23 12:34:54 -0300

    Added Inputs and adjusted the initial spaceship position
    - Implemented the move right logic;
    - Implemented the move left logic;
    - Initial implementation of the 'A' button logic.

--- naveZika.asm
+++ naveZika.asm
@@ -10,6 +10,21 @@
 pointerBackgroundLowByte  .rs 1
 pointerBackgroundHighByte .rs 1
 
+; These variables represent the vertical and horizontal positions of our spaceship sprites; 
+shipTile1Y = $0300          
+shipTile2Y = $0304
+shipTile3Y = $0308
+shipTile4Y = $030C
+shipTile5Y = $0310
+shipTile6Y = $0314
+
+shipTile1X = $0303
+shipTile2X = $0307
+shipTile3X = $030B
+shipTile4X = $030F
+shipTile5X = $0313
+shipTile6X = $0317
+
   .bank 0 ; Defines bank of memory
   .org $C000 ; Defines where in the CPU’s address space it is located
 
@@ -36,6 +51,19 @@ clrmem:
   BNE clrmem
 
 LoadBackground:
+  LDA $2002
+  LDA #$20
+  STA $2006
+  LDA #$00
+  STA $2006
+
+  LDA #LOW(background)
+  STA pointerBackgroundLowByte
+  LDA #HIGH(background)
+  STA pointerBackgroundHighByte
+
+  LDX #$00
+  LDY #$00
 
 LoadPalettes:
   LDA $2002 ; read PPU status to reset the high/low latch
@@ -79,12 +107,85 @@ LoadSprites:
   BNE .Loop         ;if x = $18, 24 in decimal, all done
   RTS
 
+ReadPlayerOneControls:
+  LDA #$01          ; prepare the controller to be read.
+  STA $4016
+  LDA#$00
+  STA $4016
+
+; Each load of the $4016 memory address is reading a different button.
+; The buttons are read always in this sequence: (A, B, Select, Start, Up, Down, Left, Right)
+
+  LDA $4016         ; Player 1 - A
+  AND #$00000001    ; If the A Button was pressed, the result of the AND operation will be 1
+  BEQ EndReadA      ; otherwise result will be 0.
+
+; TODO:   Implement the logic to be used when the 'A' button be pressed.
+;         This button will be used to fire a bullet from the spaceship.
+EndReadA:
+
+  LDA $4016         ; Player 1 - B
+  LDA $4016         ; Player 1 - Select
+  LDA $4016         ; Player 1 - Start 
+  LDA $4016         ; Player 1 - Up
+  LDA $4016         ; Player 1 - Down
+
+ ReadLeft:
+  LDA $4016         ; Player 1 - Left
+  AND #$00000001    ; If the Left Button was pressed, the result of the AND operation will be 1
+  BEQ EndReadLeft   ; otherwise result will be 0.
+
+  LDA shipTile1X
+  SEC
+  SBC #01
+  STA shipTile1X
+  STA shipTile4X
+
+  LDA shipTile2X
+  SEC
+  SBC #01
+  STA shipTile2X
+  STA shipTile5X
+
+  LDA shipTile3X
+  SEC
+  SBC #01
+  STA shipTile3X
+  STA shipTile6X
+EndReadLeft:
+
+ReadRight:
+  LDA $4016         ; Player 1 - Right
+  AND #$00000001    ; If the Right Button was pressed, the result of the AND operation will be 1
+  BEQ EndReadRight  ; otherwise result will be 0.
+
+  LDA shipTile1X
+  CLC
+  ADC #01
+  STA shipTile1X
+  STA shipTile4X
+
+  LDA shipTile2X
+  CLC
+  ADC #01
+  STA shipTile2X
+  STA shipTile5X
+
+  LDA shipTile3X
+  CLC
+  ADC #01
+  STA shipTile3X
+  STA shipTile6X
+EndReadRight:
+
 NMI:
   LDA #$00
   STA $2003 ; set the low byte (00) of the RAM address
   LDA #$03
   STA $4014 ; set the high byte (02) of the RAM address, start the transfer
   RTI       ; return from interrupt
+
+  JSR ReadPlayerOneControls
   
   .bank 1
   .org $E000
