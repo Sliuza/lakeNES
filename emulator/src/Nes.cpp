b7603b730c2b5884d03c68a12589b59cedc31151
Jean Thomé <jtc.jean@gmail.com>
2019-11-20 17:56:07 -0300

    Fixing frame rate

--- emulator/src/Nes.cpp
+++ emulator/src/Nes.cpp
@@ -3,7 +3,6 @@ const int TOTAL_CYCLES = 29781;
 #include <ctime>
 #include <set>
 
-
 Nes::Nes(string pathToRom) {
   this->cpu = Cpu();
   this->ppu = Ppu();
@@ -16,7 +15,7 @@ void Nes::load() {
   ppu.writeTblPattern();
   cpu.setPpu(&ppu);
   ppu.setCpu(&cpu);
-  ppu.set_nmi_callback([&](){ cpu.nmi_interruption(); });
+  ppu.set_nmi_callback([&]() { cpu.nmi_interruption(); });
   cpu.startCpu();
   ppu.startPpu();
 }
@@ -25,16 +24,28 @@ void Nes::run() {
   std::clock_t start;
   double duration;
 
+  uint32_t frameStart, frameTime;
+  const int FPS = 60;
+  const int DELAY = 1000.0f / FPS;
+
   while (!cpu.getFoundBrk()) {
-    start = std::clock();  
+    start = std::clock();
+
+    frameStart = SDL_GetTicks();
     for (int i = 0; i < TOTAL_CYCLES && !cpu.getFoundBrk(); i++) {
       this->runCycle();
     }
     ppu.renderize();
     cpu.nmi_interruption();
-    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-    std::cout<<"frame/s: "<< 1/duration <<'\n';
-    cout << "avr instruction time:" << cpu.avr_time_instruction << endl;
+
+    frameTime = SDL_GetTicks() - frameStart;
+    if (frameTime < DELAY) {
+      SDL_Delay((int)(DELAY - frameTime));
+    }
+
+    duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
+    // std::cout << "frame/s: " << 1 / duration << '\n';
+    // cout << "avr instruction time:" << cpu.avr_time_instruction << endl;
     //TODO: Integrate with the ppu.renderize();
   };
   ppu.endPpu();
